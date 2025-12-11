2597d893ca70016a8692ea724e169337c87093f2
Dru C. Romano Abello <dru.abello@Mas-MacBook-Air.home>
2025-12-11 23:43:18 +0100

    Add hover effect over squares

--- chess.js
+++ chess.js
@@ -16,8 +16,20 @@ for (let row = 0; row < 8; row++) {
 	}
 }
 
-const grid = [];
 
+//grid with elements
+const grid = [];
 for (let i = 0; i < 64; i++) {
 	grid.push(chessboard.children[i]);
 }
+
+//when hovering over a square, be turn to a darker colour
+
+//assign values to chess pieces
+const pawn = 1;
+const knight = 2;
+const bishop = 3;
+const rook = 5;
+const queen = 9;
+
+//notater
