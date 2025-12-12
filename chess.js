c15dd383df76963911cf65288149a1001254f8ae
Dru C. Romano Abello <dru.abello@Mas-MacBook-Air.local>
2025-12-12 20:42:36 +0100

    Add draggable function

--- chess.js
+++ chess.js
@@ -23,13 +23,50 @@ for (let i = 0; i < 64; i++) {
 	grid.push(chessboard.children[i]);
 }
 
-//when hovering over a square, be turn to a darker colour
 
-//assign values to chess pieces
-const pawn = 1;
+//make element draggable function
+const pawn = document.getElementById("pawn");
+
+function dragElement(elmnt) {
+	var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  
+	// Use mousedown, not mouseover
+	elmnt.onmousedown = dragMouseDown;
+  
+	function dragMouseDown(e) {
+	  e = e || window.event;
+	  e.preventDefault();
+	  pos3 = e.clientX;
+	  pos4 = e.clientY;
+	  document.onmouseup = closeDragElement;
+	  document.onmousemove = elementDrag;
+	}
+  
+	function elementDrag(e) {
+	  e = e || window.event;
+	  e.preventDefault();
+	  pos1 = pos3 - e.clientX;
+	  pos2 = pos4 - e.clientY;
+	  pos3 = e.clientX;
+	  pos4 = e.clientY;
+	  elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+	  elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+	}
+  
+	function closeDragElement() {
+	  document.onmouseup = null;
+	  document.onmousemove = null;
+	}
+  }
+
+dragElement(pawn);
+  
+
+//assign values to chess piece
 const knight = 2;
 const bishop = 3;
 const rook = 5;
 const queen = 9;
+
 
 //notater
