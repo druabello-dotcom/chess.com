8b7847b6cedc54a2eb444946c614f3a39b69d6da
Dru C. Romano Abello <druabello@gmail.com>
2026-01-20 23:28:25 +0100

    AUTOMATE: selected color indicator

--- chess.js
+++ chess.js
@@ -236,11 +236,14 @@ select.addEventListener('click', () => {
 // choose colorway
 let colorwayArray = [];
 const colorwayElements = Array.from(document.querySelectorAll('.options span'));
+let selectIndicator = document.querySelector('.selected');
 for (let i = 1; i <= colorwayElements.length; i++) colorwayArray.push(i);
 for (let i = 0;  i < colorwayArray.length; i++) {
 	colorwayElements[i].addEventListener('click', (event) => {
 		let selectedColorway = event.target.id;
 		chessboard.className = selectedColorway;
+		selectIndicator.innerText = selectedColorway;
+
 	})
 }
 
