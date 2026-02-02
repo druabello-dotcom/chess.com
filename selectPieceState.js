2b536c6561b47594e98f06eac8c8509ca793cfc7
Dru C. Romano Abello <druabello@gmail.com>
2026-02-02 16:53:39 +0100

    MAKE FILE: store selected piece state variables in different file

---
+++ selectPieceState.js
@@ -0,0 +1,17 @@
+export const selectPieceState = {
+    isClicked: false,
+    selectedSquare: null,
+    selectedSquareId: null,
+    destinationSquare: null,
+    clickOnPieceToReset: [],
+
+    selectedPiece: null,
+    selectedPieceArray: null,
+    selectedPieceIndex: null,
+    pieceType: null,
+    valueInSquare: null,
+    pieceColor: null,
+
+    x_squareCoordinate: null,
+    y_squareCoordinate: null,
+}
