47c3cbee5ab08544e317b2fa1ae84a1d167c6ba4
Dru C. Romano Abello <druabello@gmail.com>
2026-02-03 18:34:24 +0100

    REFINE FILE: onSquareClick.js

--- onSquareClick.js
+++ onSquareClick.js
@@ -1,5 +1,8 @@
 import * as Main from "./main.js";
-import { selectPieceState } from "./selectPieceState.js";
+import * as TurnRegister from "./turnRegister.js";
+import { selectPieceState, pieceSquarePositionArray, pieceElementsObject } from "./gameState.js";
+import { moveToDestination } from "./movePieceToDestination.js";
+import { availablePieceMovesObject } from "./createAvailableMoves.js";
 
 export function onSquareClick(event) {
 	selectPieceState.selectedSquare = event.target;
@@ -14,21 +17,21 @@ export function onSquareClick(event) {
 	if (selectPieceState.valueInSquare < 0) selectPieceState.pieceColor = 'black';
 	else if (0 < selectPieceState.valueInSquare) selectPieceState.pieceColor = 'white';
 	console.log("Color of piece:  " + selectPieceState.pieceColor);
-	if (selectPieceState.pieceColor != Main.turnDecider) return; // same player can't move twice in a row
+	if (selectPieceState.pieceColor != TurnRegister.registerTurnVariables.turnDecider) return; // same player can't move twice in a row
 
 	selectPieceState.isClicked = true;
 	selectPieceState.selectedSquare.style.filter = "brightness(0.4)";
 
 	// get information about piece
 	selectPieceState.pieceType = Main.mapPieces[Math.abs(selectPieceState.valueInSquare)];
-	selectPieceState.selectedPieceArray = Main.pieceSquarePositionArray[selectPieceState.pieceColor][selectPieceState.pieceType];
+	selectPieceState.selectedPieceArray = pieceSquarePositionArray[selectPieceState.pieceColor][selectPieceState.pieceType];
 	console.log(selectPieceState.selectedPieceArray);
 	console.log("You must move:  " + selectPieceState.pieceType)
 	selectPieceState.selectedPieceIndex = selectPieceState.selectedPieceArray.indexOf(selectPieceState.selectedSquareId);
 	console.log(selectPieceState.selectedPieceIndex);
 
 	// the selected piece is now found inside program
-	selectPieceState.selectedPiece = Main.pieceElementsObject[selectPieceState.pieceColor][selectPieceState.pieceType][selectPieceState.selectedPieceIndex];
+	selectPieceState.selectedPiece = pieceElementsObject[selectPieceState.pieceColor][selectPieceState.pieceType][selectPieceState.selectedPieceIndex];
 	console.log(selectPieceState.selectedPiece);
 
 	// add eventListeners for available square for corresponding piece
@@ -36,12 +39,13 @@ export function onSquareClick(event) {
 	for (let i = 0; i < 64; i++) {
 		Main.grid[i].removeEventListener('click', onSquareClick);
 		if (Main.stateGrid[i] < 0 && selectPieceState.pieceColor === 'black') { // 
-			Main.grid[i].addEventListener('click', Main.moveToDestination);
+			Main.grid[i].addEventListener('click', moveToDestination);
 			selectPieceState.clickOnPieceToReset.push(i);
 		} else if (0 < Main.stateGrid[i] && selectPieceState.pieceColor === 'white') {
-			Main.grid[i].addEventListener('click', Main.moveToDestination);
+			Main.grid[i].addEventListener('click', moveToDestination);
 			selectPieceState.clickOnPieceToReset.push(i);
 		} 
 	}
-	Main.availablePieceMovesObject[selectPieceState.pieceType]();
+	console.log("Click on piece to reset:  " +selectPieceState.clickOnPieceToReset)
+	availablePieceMovesObject[selectPieceState.pieceType]();
 }
