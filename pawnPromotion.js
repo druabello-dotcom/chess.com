d66da5d10e504700a07ebdf444ff8e7639b12eca
Dru C. Romano Abello <druabello@gmail.com>
2026-02-04 20:34:43 +0100

    MAKE OBJECT: store chessboard square coordinates

--- pawnPromotion.js
+++ pawnPromotion.js
@@ -3,3 +3,10 @@ import * as CreatePieceElements from "./createPieceElements.js";
 import { pieceNumberIdentifier, pieceSquarePositionArray, selectPieceState } from "./gameState.js";
 import { makeKingCastle } from "./makeKingCastle.js";
 
+let squareDivision = [];
+let pixelCoordinatesForSquares = (chessboardBoard.chessboardDimentions.width / 8);
+for (let row = 0; row < 8; row++) {
+    for (let column = 0; column < 9; column++) {
+        squareDivision.push({x_coordinate: pixelCoordinatesForSquares * column, y_coordinate: pixelCoordinatesForSquares * row})
+    }
+}
