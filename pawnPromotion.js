17bd2541d78f5ce8f9b398bde34688035ea1ff4b
Dru C. Romano Abello <druabello@gmail.com>
2026-02-07 19:57:48 +0100

    FUNCTION: show promotion options

--- pawnPromotion.js
+++ pawnPromotion.js
@@ -3,10 +3,16 @@ import * as CreatePieceElements from "./createPieceElements.js";
 import { pieceElementsObject, pieceNumberIdentifier, pieceSquarePositionArray, selectPieceState } from "./gameState.js";
 import { makeKingCastle } from "./makeKingCastle.js";
 
-
+/* let squareDivision = [];
+for (let row = 0; row < 8; row++) {
+    for (let column = 0; column < 9; column++) {
+        squareDivision.push({x_coordinate: pixelCoordinatesSquares * column, y_coordinate: pixelCoordinatesSquares * row})
+    }
+} */
 
 //————————————————————————————————————————————————————————————————————————————————————
 
+let promotionOptions = null;
 export function promotePawn(destinationSquare) {
     let squareDivision = [];
     let chessboard = document.getElementById('chessboard');
@@ -20,18 +26,30 @@ export function promotePawn(destinationSquare) {
         for (let column = 0, row = 7; column < 8; column++) squareDivision.push({x_coordinate: pixelCoordinatesSquares * column, y_coordinate: pixelCoordinatesSquares * row});
         optionsIndex = 56;
     }
-    console.log(squareDivision);
-
 
     // create promotion options visually
-    let promotionOptions = document.createElement('div');
+    promotionOptions = document.createElement('div');
     promotionOptions.classList.add('white');
     promotionOptions.classList.add('promotionOptions');
 
     // promotion options placement on chessboard
     promotionOptions.style.left = parseInt(squareDivision[destinationSquare - optionsIndex].x_coordinate) + "px";
     promotionOptions.style.top = parseInt(squareDivision[destinationSquare - optionsIndex].y_coordinate) + "px";
-    promotionOptions.style.position = "absolute";
-    promotionOptions.style.height = "100px";
+    showPromotionOptions(selectPieceState.pieceColor);
+    
     chessboard.appendChild(promotionOptions);
+}
+
+function showPromotionOptions(color) {
+    for (let t = 1; t < (CreatePieceElements.pieceTypeArray.length - 1); t++) {
+        let type = CreatePieceElements.pieceTypeArray[t];
+        let optionSpan = document.createElement('span');
+        let optionImg =document.createElement('img');
+        optionSpan.classList.add('piece', color, type);        
+        optionImg.src = CreatePieceElements.pieceIcons[color][type];
+        optionImg.alt = CreatePieceElements.pieceIconAlt[color][type];
+
+        optionSpan.appendChild(optionImg);
+        promotionOptions.appendChild(optionSpan);
+    }
 }
