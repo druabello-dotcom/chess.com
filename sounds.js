0992987a68416ec260765bcc25dcb1b74b67cd3d
Dru C. Romano Abello <druabello@gmail.com>
2026-02-12 09:14:33 +0100

    MAKE FUNCTION: capture sound

--- sounds.js
+++ sounds.js
@@ -2,3 +2,8 @@ export function soundWhenMovingPiece() {
 	let movingPieceSound = new Audio("sounds_media/move-self.mp3");
 	movingPieceSound.play();
 }
+
+export function captureSound() {
+	let captureSound = new Audio("sounds_media/capture.mp3");
+	captureSound.play();
+}
