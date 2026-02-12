e3f58e2ebe63be18bfc5695fb3d2e08a70fb62c8
Dru C. Romano Abello <druabello@gmail.com>
2026-02-12 10:03:50 +0100

    ADD FILES: all sound files

--- sounds.js
+++ sounds.js
@@ -1,3 +1,6 @@
+// LINK TO ALL SOUND FILES: 
+// https://www.chess.com/forum/view/general/chessboard-sound-files?page=2
+
 export function soundWhenMovingPiece() {
 	let movingPieceSound = new Audio("sounds_media/move-self.mp3");
 	movingPieceSound.play();
@@ -11,4 +14,29 @@ export function captureSound() {
 export function castleSound() {
 	let castleSound = new Audio("sounds_media/castle.mp3");
 	castleSound.play();
+}
+
+export function endGame() {
+	let endGameSound = new Audio("sounds_media/game-end.mp3");
+	endGameSound.play();
+}
+
+export function giveCheckSound() {
+	let checkSound = new Audio("sounds_media/give-check.mp3")
+	checkSound.play();
+}
+
+export function promotionSound() {
+	let promoteSound = new Audio("sounds_media/promote.mp3");
+	promoteSound.play();
+}
+
+export function illegalMove() {
+	let illegalMoveSound = new Audio("sounds_media/illegal-move.mp3");
+	illegalMoveSound.play();
+}
+
+export function lowTime() {
+	let lowTimeSound = new Audio("sounds_media/low-time.mp3");
+	lowTimeSound.play();
 }
